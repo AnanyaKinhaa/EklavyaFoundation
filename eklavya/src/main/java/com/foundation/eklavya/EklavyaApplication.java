6fed141ac6b47fedc2a1ffe7e2ba78e5db70a5c8
Ananya Kinha <ananyakinha@gmail.com>
2024-06-18 12:37:40 +0530

    Spring Initializer

---
+++ eklavya/src/main/java/com/foundation/eklavya/EklavyaApplication.java
@@ -0,0 +1,13 @@
+package com.foundation.eklavya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EklavyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EklavyaApplication.class, args);
+	}
+
+}
---
+++ eklavya/src/test/java/com/foundation/eklavya/EklavyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foundation.eklavya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EklavyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
